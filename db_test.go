ac7c47e0cf6ac480ce15d3d916756730f3650ecf
Sudhir Jonathan <sudhir.j@gmail.com>
2024-05-25 21:04:27 +0530

    More testing

--- db_test.go
+++ db_test.go
@@ -28,6 +28,38 @@ func TestHashes(t *testing.T) {
 	kvMap, err := rdb.HGETALL("k1")
 	assert.NoError(t, err)
 	assert.Equal(t, "v1", kvMap["f1"])
+
+	err = rdb.HSET("k1", "f2", "v2")
+	assert.NoError(t, err)
+
+	err = rdb.HSET("k2", "f1", "v1")
+	assert.NoError(t, err)
+
+	keys, err := rdb.HKEYS("k1")
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []string{"f1", "f2"}, keys)
+
+	count, err := rdb.HLEN("k1")
+	assert.NoError(t, err)
+	assert.Equal(t, 2, count)
+
+	err = rdb.HDEL("k1", "f1")
+	assert.NoError(t, err)
+
+	keys, err = rdb.HKEYS("k1")
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []string{"f2"}, keys)
+
+	count, err = rdb.HLEN("k1")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, count)
+
+	err = rdb.HDELALL("k1")
+	assert.NoError(t, err)
+
+	keys, err = rdb.HKEYS("k1")
+	assert.NoError(t, err)
+	assert.Empty(t, keys)
 }
 
 func makeTestDB(t *testing.T) *bolt.DB {
